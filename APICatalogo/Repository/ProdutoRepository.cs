12328d8edf4d07cf25066a6cb7a7f9f66f223855
Pedro Lucas <pedrolucas122737@gmail.com>
2023-11-19 20:29:38 -0300

    Padrão repositório e implementação

---
+++ APICatalogo/Repository/ProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiCatalogo.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Produto> GetProdutoPorPreco()
+        {
+            return Get().OrderBy(c => c.Preco).ToList();
+        }
+    }
+}
